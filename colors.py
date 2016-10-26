38cbd4fcea9e14d5970ec959df6457997c8472e5
kevinpanaro <panaro.kevin@gmail.com>
2016-10-25 23:37:27 -0400

    optimize publish, rename some vars

--- colors.py
+++ colors.py
@@ -34,60 +34,54 @@ RGB_COMMAND_TOPIC = b'led/rgb/set'
 
 # Pin for NODEMCU and Neopixel
 LED_PIN = 5
-PIN = Pin(LED_PIN, Pin.OUT)
-NEOPIXEL = NeoPixel(PIN, 6)
-LIGHT_BRIGHTNESS = 255
-STATE = 0
 NUM_OF_LEDS = 6
-OLD = (0,0,0)
+PIN = Pin(LED_PIN, Pin.OUT)
+NEOPIXEL = NeoPixel(PIN, NUM_OF_LEDS)
+BRIGHTNESS_STATE = 0
+STATE = 0       # I don't know but this is really important for some reason.
+RGB_STATE = (0,0,0)
 
-def publish_light_state():
-    if STATE == 1:
-        c.publish(STATE_TOPIC, "ON")
-        print("Publish ON to %s" % STATE_TOPIC)
-    else:
-        c.publish(STATE_TOPIC, "OFF")
-        print("Publish OFF to %s" % STATE_TOPIC)
-
-def publish_rgb_brightness():
-    c.publish(BRIGHTNESS_STATE_TOPIC, str(LIGHT_BRIGHTNESS))
-    print("Publish %s to %s" % (str(LIGHT_BRIGHTNESS), BRIGHTNESS_STATE_TOPIC))
-
-def publish_rgb_state(colors):
-    colors = ",".join(colors)
-    c.publish(RGB_STATE_TOPIC, colors, retain=True)
-    print("Publish %s to %s" % (colors, RGB_STATE_TOPIC))
+def publish_payload(topic, payload):
+    '''publish to a topic with a payload'''
+    c.publish(topic, str(payload))
+    print("[Publish]\t%s => %s" % (topic, str(payload)))
 
 def set_led(on=True, brightness=255, color=False):
     '''on is True or False
     brightness is 0-255
     color is list [red, green, blue]
     '''
-    global OLD
+    global RGB_STATE
     if not on:
         new = (0, 0, 0)
-        transition(OLD, new, 50)
+        smooth(RGB_STATE, new, 50)
     elif on:
         if color:
             red = int(color[0])
             green = int(color[1])
             blue = int(color[2])
             new = (red, green, blue)
-            transition(OLD, new, 75)
+            smooth(RGB_STATE, new, 75)
         else:
             new = (brightness, brightness, brightness)
-            transition(OLD, new, 75)
+            smooth(RGB_STATE, new, 75)
 
-def transition(old, new, trasnition):
-    global OLD
-    red = list(linspace(old[0], new[0], trasnition))
-    green = list(linspace(old[1], new[1], trasnition))
-    blue = list(linspace(old[2], new[2], trasnition))
+def smooth(old, new, smooth, transition=0):
+    '''old is deprecated, probably going to be replaced by RGB_STATE
+    new is the new color (RED, GREEN, BLUE)
+    smooth is how many "frames" of color to create and display
+    transition is seconds to transit from one color to the next
+    transition is 0 right now, but will be implimented with MQTT JSON'''
+    global RGB_STATE
+    red = list(linspace(RGB_STATE[0], new[0], smooth))
+    green = list(linspace(RGB_STATE[1], new[1], smooth))
+    blue = list(linspace(RGB_STATE[2], new[2], smooth))
     for color in zip(red,green,blue):
         for led in range(NUM_OF_LEDS):
             NEOPIXEL[led] = color
         NEOPIXEL.write()
-    OLD = new
+    RGB_STATE = new
+
 
 def linspace(a,b, n=100):
     if n < 2:
@@ -98,44 +92,39 @@ def linspace(a,b, n=100):
 
 def sub_cb(topic, msg):
     global STATE
-    # global OLD
-    print((topic, msg))
     if topic == COMMAND_TOPIC:
         if msg == b"ON":
             if STATE == 0:
                 STATE = 1
                 set_led(on=True, brightness=255, color=False)
-                publish_light_state()
-                publish_rgb_brightness()
+                publish_payload(STATE_TOPIC, "ON")
+                publish_payload(BRIGHTNESS_STATE_TOPIC, 255)
         elif msg == b"OFF":
             if STATE == 1:
                 STATE = 0
-                LIGHT_BRIGHTNESS = 0
+                BRIGHTNESS_STATE = 0
                 set_led(on=False)
-                publish_light_state()
-                publish_rgb_brightness()
+                publish_payload(STATE_TOPIC, "OFF")
+                publish_payload(BRIGHTNESS_STATE_TOPIC, 0)
     elif topic == BRIGHTNESS_COMMAND_TOPIC:
         brightness = int(msg)
         if brightness < 0 or brightness > 255:
             return
         else:
             set_led(on=True, brightness=brightness, color=False)
-            publish_rgb_brightness()
+            publish_payload(BRIGHTNESS_STATE_TOPIC, brightness)
     elif topic == RGB_COMMAND_TOPIC:
         colors = msg.decode("utf-8").split(',')
-        try:
-            set_led(on=True, brightness=LIGHT_BRIGHTNESS, color=colors)
-        except NameError:
-            LIGHT_BRIGHTNESS = 255
-            set_led(on=True, brightness=LIGHT_BRIGHTNESS, color=colors)
-        publish_rgb_state(colors)
+        set_led(on=True, brightness=False, color=colors)
+        publish_payload(RGB_STATE_TOPIC, ",".join(colors))
         
 
 def reconnect():
     c.set_callback(sub_cb)
     c.connect()
-    publish_light_state()
-    publish_rgb_brightness()
+    publish_payload(STATE_TOPIC, STATE)
+    publish_payload(BRIGHTNESS_STATE_TOPIC, BRIGHTNESS_STATE)
+    publish_payload(RGB_STATE_TOPIC, RGB_STATE)
     c.subscribe(COMMAND_TOPIC)
     c.subscribe(BRIGHTNESS_COMMAND_TOPIC)
     c.subscribe(RGB_COMMAND_TOPIC)
@@ -149,11 +138,9 @@ def main(server=SERVER):
         wlan.connect(SSID, PASS)
         while not wlan.isconnected():
             pass
-    print('network config:', wlan.ifconfig())
+    print('[Network Config]\n ', wlan.ifconfig())
     c = MQTTClient(CLIENT_ID, server)
-    # Subscribed messages will be delivered to this callback
     reconnect()
-    print("Connected to %s, subscribed to %s topic" % (server, COMMAND_TOPIC))
     try:
         while True:
             if not wlan.isconnected():
