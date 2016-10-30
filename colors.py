cac1a855fa5f68ca88a9583de5f76806887d2d47
kevinpanaro <panaro.kevin@gmail.com>
2016-10-30 19:26:29 -0400

    optimize code, start adding scene capabilities

--- colors.py
+++ colors.py
@@ -8,6 +8,7 @@ from sys import exit
 import ubinascii
 import machine
 import network
+import police
 # ESP8266 ESP-12 modules have blue, active-low LED on GPIO2, replace
 # with something else if needed.
 '''
@@ -30,11 +31,11 @@ switch:
     payload_off: "dead"
 '''
 # Wifi 
-SSID = ""
-PASS = ""
+SSID = "StewNkrew"
+PASS = "weinersz"
 
 # MQTT server to connect to 
-SERVER = "172.16.0.30" 
+SERVER = "192.168.0.10" 
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 STATE_TOPIC= b"led/status"
 COMMAND_TOPIC = b"led/switch"
@@ -42,8 +43,12 @@ BRIGHTNESS_STATE_TOPIC = b'led/brightness/status'
 BRIGHTNESS_COMMAND_TOPIC = b'led/brightness/set'
 RGB_STATE_TOPIC = b'led/rgb/status'
 RGB_COMMAND_TOPIC = b'led/rgb/set'
+
+# extra topics
 KILL_COMMAND_TOPIC = b'led/kill'
 KILL_STATE_TOPIC = b'led/alive'
+SCENES_COMMAND_TOPIC = b'led/scene/set'
+SCENES_STATE_STOPIC = b'leb/scene/status'
 
 # Pin for NODEMCU and Neopixel
 LED_PIN = 5
@@ -72,18 +77,7 @@ def get_rgb_value(color, new_value):
     r, g, b = [int(round(color*255)) for color in htr(h, s, new_value)]
     return r, g, b
 
-def set_led(on=False, color=(0,0,0)):
-    '''
-    on is True or False
-    color is list [red, green, blue]
-    '''
-    if not on:
-        smooth(color, 50)
-    elif on:
-        if color:
-            smooth(color, 75)
-
-def smooth(new, smooth, transition=0):
+def set_led(new=(0,0,0), smooth=50, transition=0):
     '''
     new is the new color (RED, GREEN, BLUE)
     smooth is how many "frames" of color to create and display
@@ -117,13 +111,13 @@ def sub_cb(topic, msg):
             RGB_STATE = (255, 255, 255)
             publish_payload(STATE_TOPIC, "ON")
             publish_payload(BRIGHTNESS_STATE_TOPIC, 255)
-            set_led(on=True, color=RGB_STATE)
+            set_led(new=RGB_STATE, smooth=75)
         elif msg == b"OFF" and STATE == "ON":
             STATE = "OFF"
             RGB_STATE = (0, 0, 0)
             publish_payload(STATE_TOPIC, "OFF")
             publish_payload(BRIGHTNESS_STATE_TOPIC, 0)
-            set_led(on=False, color=RGB_STATE)
+            set_led()
     elif topic == BRIGHTNESS_COMMAND_TOPIC:
         brightness = int(msg)
         if brightness < 0 or brightness > 255:
@@ -132,14 +126,14 @@ def sub_cb(topic, msg):
             colors = tuple(get_rgb_value(RGB_STATE, brightness/255.0))
             publish_payload(BRIGHTNESS_STATE_TOPIC, brightness)
             publish_payload(RGB_STATE_TOPIC, ",".join([str(color) for color in colors]))
-            set_led(on=True, color=colors)
+            set_led(new=colors, smooth=75)
     elif topic == RGB_COMMAND_TOPIC:
         colors = msg.decode("utf-8").split(',')
         RGB_STATE = colors
         _, _, v = get_hsv_value(colors)
         publish_payload(BRIGHTNESS_STATE_TOPIC, int(v*255))
         publish_payload(RGB_STATE_TOPIC, ",".join(colors))
-        set_led(on=True, color=tuple([int(color) for color in colors]))
+        set_led(new=tuple([int(color) for color in colors]), smooth=75)
     elif topic == KILL_COMMAND_TOPIC:
         if msg == b'dead':
             publish_payload(STATE_TOPIC, "OFF")
@@ -149,6 +143,12 @@ def sub_cb(topic, msg):
             exit()
         else:
             publish_payload(KILL_STATE_TOPIC, "alive")
+    elif topic == SCENES_COMMAND_TOPIC:
+        if msg == b'police':
+            police.police()
+            publish_payload(STATE_TOPIC, "ON")
+            publish_payload(BRIGHTNESS_STATE_TOPIC, 255)
+            set_led()
 
 def reconnect():
     c.set_callback(sub_cb)
@@ -161,6 +161,7 @@ def reconnect():
     c.subscribe(BRIGHTNESS_COMMAND_TOPIC)
     c.subscribe(RGB_COMMAND_TOPIC)
     c.subscribe(KILL_COMMAND_TOPIC)
+    c.subscribe(SCENES_COMMAND_TOPIC)
 
 def main(server=SERVER):
     global c
